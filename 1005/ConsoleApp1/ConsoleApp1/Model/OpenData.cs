7c015775a40dfd98636856e35c310e08d53560f8
黃宥瑔 <1105105335@office365.kuas.edu.tw>
2018-10-07 19:38:03 +0800

    HYC ADD

---
+++ 1005/ConsoleApp1/ConsoleApp1/Model/OpenData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Model
+{
+    class OpenData
+    {
+
+        public string REGI_ID { get; set; }
+
+        public string FACT_NAME { get; set; }
+
+        public string FACT_ADDR { get; set; }
+
+        public string BNAME { get; set; }
+
+        public string ADDR_X { get; set; }
+
+        public string ADDR_Y { get; set; }
+    }
+}
