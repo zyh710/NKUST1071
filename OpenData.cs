de72baddf941fbeefcf571eb0b2a07e2994f2c1a
Carter870214 <43459731+Carter870214@users.noreply.github.com>
2018-10-11 21:57:51 +0800

    1105105348 1011add

---
+++ OpenData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenDataImport.Models
+{
+    public class OpenData
+    {
+        public string 班別名稱 { get; set; }
+        public string 辦理區域 { get; set; }
+        public string 上課地點學科 { get; set; }
+        public string 訓練起日 { get; set; }
+        public string 訓練迄日 { get; set; }
+    }
+}
