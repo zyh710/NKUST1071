b16920f225d32d1e96e434fd2843395c27abc853
YinChang <yinchang0626@gmail.com>
2018-10-19 12:03:03 +0800

    done

--- 1018/資料庫操作/OpenDataImport/Program.cs
+++ 1018/資料庫操作/OpenDataImport/Program.cs
@@ -11,51 +11,19 @@ namespace OpenDataImport
     {
         static void Main(string[] args)
         {
-            var nodes=findOpenData();
+            Service.ImportService importService = new Service.ImportService();
+
+            var nodes= importService.FindOpenData();
+
+            importService.ImportToDb(nodes);
+
             showOpenData(nodes);
             Console.ReadKey();
 
         }
-        public void Configure()
-        {
-            string baseDir = Directory.GetCurrentDirectory();
 
-            AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Path.Combine(baseDir, "App_Data"));
-        }
-        static List<OpenData> findOpenData()
-        {
-            List<OpenData> result = new List<OpenData>();
+        
 
-            string baseDir = Directory.GetCurrentDirectory();
-
-
-            var xml = XElement.Load(System.IO.Path.Combine(baseDir, "App_Data/datagovtw_dataset_20181005.xml"));
-
-
-            //XNamespace gml = @"http://www.opengis.net/gml/3.2";
-            //XNamespace twed = @"http://twed.wra.gov.tw/twedml/opendata";
-            var nodes = xml.Descendants("node").ToList();
-
-
-            result = nodes
-                .Where(x => !x.IsEmpty).ToList()
-                .Select(node =>
-                {
-                    OpenData item = new OpenData();
-                    item.id =int.Parse(getValue(node, "id"));
-                    item.資料集名稱 = getValue(node, "資料集名稱");
-                    item.服務分類 = getValue(node, "服務分類");
-                    item.主要欄位說明 = getValue(node, "主要欄位說明");
-                    return item;
-                }).ToList();
-            return result;
-
-        }
-        private static string getValue(XElement node, string propertyName)
-        {
-            return node.Element(propertyName)?.Value?.Trim();
-
-        }
 
 
         private static void showOpenData(List<OpenData> nodes)
