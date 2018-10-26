7669a79c9fde6e80a3aa24e0b3217a1545736595
YinChang <yinchang0626@gmail.com>
2018-10-26 08:24:35 +0800

    合併完點名，加入1026資料夾

---
+++ 1026/資料庫操作/OpenDataImport/Models/OpenData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenDataImport.Models
+{
+    public class OpenData
+    {
+
+
+
+
+        public int id { get; set; }
+        public string 資料集名稱 { get; set; }
+        public string 主要欄位說明 { get; set; }
+        public string 服務分類 { get; set; }
+
+
+
+    }
+}
