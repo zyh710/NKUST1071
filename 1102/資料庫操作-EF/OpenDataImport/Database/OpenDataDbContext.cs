ab4e15593ee1b5650733975598330ad7d30d3ee4
YinChang <yinchang0626@gmail.com>
2018-11-02 11:39:01 +0800

    Add EF's dbcontext

---
+++ 1102/資料庫操作-EF/OpenDataImport/Database/OpenDataDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenDataImport.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace OpenDataImport.Database
+{
+    public class OpenDataDbContext:DbContext
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Projects\NKUST1071\1102\資料庫操作-EF\OpenDataImport\App_Data\Database.mdf;Integrated Security=True";
+                
+            }
+
+        }
+
+        public DbSet<OpenData> OpenData { get; set; }
+
+
+
+        public OpenDataDbContext() : base()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseSqlServer(ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
