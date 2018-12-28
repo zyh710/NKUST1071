fef763b2994da1d46d35da236bf337b55a37794f
YinChang <yinchang0626@gmail.com>
2018-12-28 10:52:32 +0800

    done

---
+++ 1228/Web-Js/src/YC.Core/Database/OpenDataDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
+using System.Text;
+using YC.Models;
+
+namespace YC.Database
+{
+    public class OpenDataDbContext:DbContext
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), @"App_Data\Database.mdf");
+
+                return $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={path};Integrated Security=True";
+                
+            }
+
+        }
+
+        public DbSet<OpenData> OpenData { get; set; }
+
+
+
+        public OpenDataDbContext() : base()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseSqlServer(ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
