e962b1cc9f2f542749f8b5ee478a0b342926fd82
Tsang <tsang6961@gmail.com>
2019-01-09 22:43:54 +0800

    ADD

---
+++ 1018/資料庫操作/HT.Web/Controllers/OpendataController.cs
@@ -0,0 +1,58 @@
+﻿using HTCore.Database;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace HT.Web.Controllers
+{
+    public class OpenDataController : Controller
+    {
+        
+            public OpenDataDbContext DbContext
+            {
+                get
+                {
+                    return this.db;
+                }
+            }
+            private OpenDataDbContext db;
+            public OpenDataController()
+            {
+                db = new OpenDataDbContext();
+            }
+            public IActionResult Index()
+            {
+                List<HTCore.Models.OpenData> models = db.OpenData.ToList();
+
+                return View(models);
+            }
+
+            public IActionResult Detail(int id, string name)
+            {
+
+                var model = db.OpenData.Find(id);
+                return View(model);
+            }
+            [HttpGet]
+            public IActionResult Edit(int id)
+            {
+                var model = db.OpenData.Find(id);
+                return View(model);
+            }
+            [HttpPost]
+            public IActionResult Edit(HTCore.Models.OpenData model)
+            {
+
+                var upDateModel = db.OpenData.Find(model.id);
+                upDateModel.園區別 = model.園區別;
+                upDateModel.廠商名稱 = model.廠商名稱;
+                upDateModel.地址 = model.地址;
+                db.SaveChanges();
+                var updated = db.OpenData.Find(model.id);
+                return View(updated);
+            }
+        }
+    }
---
+++ 1018/資料庫操作/HTCore/Database/OpenDataDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using HTCore.Models;
+
+namespace HTCore.Database
+{
+    public class OpenDataDbContext : DbContext
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user\Source\Repos\NKUST10712\1018\資料庫操作\OpenDataImport\App_Data\Database1.mdf;Integrated Security=True";
+
+            }
+
+        }
+
+        public DbSet<OpenData> OpenData { get; set; }
+
+
+
+        public OpenDataDbContext() : base()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
